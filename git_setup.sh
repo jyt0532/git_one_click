280d71f028ff62ff95bbe2c18170d66100cf6b7a
bychiang <bychiang@yahoo-inc.com>
2015-10-23 22:36:50 +0000

    Add git alias and autocomplete

---
+++ git_setup.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+#git alias
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
+
+#Auto complete
+cd ~
+curl -OL https://github.com/git/git/raw/master/contrib/completion/git-completion.bash
+mv git-completion.bash .git-completion.bash
+	## in .bash_profile, add:
+	## if [ -f ~/.git-completion.bash ]; then
+  	##. ~/.git-completion.bash
+	##fi
+

354008834a07230b9e2ec7e9637aa155232f5ac0
bychiang <bychiang@yahoo-inc.com>
2016-01-05 23:58:12 +0000

    Add fancy git log

--- git_setup.sh
+++ git_setup.sh
@@ -5,6 +5,7 @@ git config --global alias.co checkout
 git config --global alias.br branch
 git config --global alias.ci commit
 git config --global alias.st status
+git config --global alias.ll 'log --oneline --graph --all --decorate'
 
 #Auto complete
 cd ~
@@ -15,3 +16,5 @@ mv git-completion.bash .git-completion.bash
   	##. ~/.git-completion.bash
 	##fi
 
+staged
+working
